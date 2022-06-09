bb800f4e1d8fd4b5bba565c6101a573e287142e8
James Jones <46056989+AceJacobi@users.noreply.github.com>
2022-06-09 09:51:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,10 +21,15 @@ document.forms[0].addEventListener('submit', (e) => {
     e.preventDefault();
 
     let special = false;
+    
     let numbers = false;
+    
     let pLength = document.getElementById('input-selector');
+    
     let specBox = document.getElementById('input-special');
+    
     let numBox = document.getElementById('input-nums');
+    
     let pString = '';
 
     if (specBox.checked) {
@@ -36,72 +41,81 @@ document.forms[0].addEventListener('submit', (e) => {
     }
 
     if (special === false && numbers === false) {
+        
         //generate plain text password
         let lengthV = parseInt(pLength.value);
 
         for (i = 0; i < lengthV; i++) {
+            
             let ranNum = Math.floor(Math.random() * 26);
-            //console.log(ranNum);
+            
             let newChoice = letters[ranNum];
-            //console.log(newChoice);
+           
             pString += newChoice;
         }
-
-        console.log(pString);
+        
         d.password = pString;
+        
     } else if (special && !numbers) {
+        
         //generate plain text w/ special chars
         let lengthV2 = parseInt(pLength.value);
 
         for (i = 0; i < lengthV2; i++) {
+            
             if (i % 2 === 0) {
-                console.log('Even number!');
+                
                 let ranNum = Math.floor(Math.random() * 26);
-                //console.log(ranNum);
+                
                 let newChoice = letters[ranNum];
-                //console.log(newChoice);
+                
                 pString += newChoice;
+                
             } else {
-                console.log('Odd number!');
+                
                 let ranNum = Math.floor(Math.random() * 7);
-                //console.log(ranNum);
+                
                 let newChoice = specialChars[ranNum];
-                //console.log(newChoice);
+                
                 pString += newChoice;
             }
         }
-        console.log(pString);
+        
         d.password = pString;
+        
     } else if (special && numbers) {
+        
         //plain text w/ nums & special
-
         let lengthV3 = parseInt(pLength.value);
 
         for (i = 0; i < lengthV3; i++) {
+            
             if (i % 2 === 0) {
-                console.log('Even number!');
+                
                 let ranNum = Math.floor(Math.random() * 26);
-                //console.log(ranNum);
+                
                 let newChoice = letters[ranNum];
-                //console.log(newChoice);
+                
                 pString += newChoice;
+                
             } else if (i % 4 === 1 || i % 4 === 2) {
-                console.log('Four multiple!');
+                
                 let ranNum = Math.floor(Math.random() * 10);
-                //console.log(ranNum);
+                
                 let newChoice = nums[ranNum];
-                //console.log(newChoice);
+                
                 pString += newChoice;
+                
             } else {
-                console.log('Odd number!');
+                
                 let ranNum = Math.floor(Math.random() * 7);
-                //console.log(ranNum);
+                
                 let newChoice = specialChars[ranNum];
-                //console.log(newChoice);
+                
                 pString += newChoice;
             }
         }
-        console.log(pString);
+        
         d.password = pString;
 
     } else {
@@ -109,19 +123,21 @@ document.forms[0].addEventListener('submit', (e) => {
         let lengthV4 = parseInt(pLength.value);
 
         for (i = 0; i < lengthV4; i++) {
+            
             if (i % 2 === 0) {
-                console.log('Even number!');
+                
                 let ranNum = Math.floor(Math.random() * 10);
-                //console.log(ranNum);
+                
                 let newChoice = nums[ranNum];
-                //console.log(newChoice);
+                
                 pString += newChoice;
             }
             else {
+                
                 let ranNum = Math.floor(Math.random() * 26);
-                //console.log(ranNum);
+
                 let newChoice = letters[ranNum];
-                //console.log(newChoice);
+                
                 pString += newChoice;
             }
         }
